1ca9c8dbd7a69b6a05e0bdd451f2b985ed26f8c8
ZhiliChen1987 <C137Chenzhili@gmail.com>
2024-12-15 11:36:50 +0800

    Create Dispose.cs

---
+++ 86BoxManager/Dispose.cs
@@ -0,0 +1,10 @@
+// 86Manager, Version=1.7.4.0, Culture=neutral, PublicKeyToken=null
+// _86boxManager.dlgSettings
+protected override void Dispose(bool disposing)
+{
+	if (disposing && components != null)
+	{
+		components.Dispose();
+	}
+	base.Dispose(disposing);
+}
